7e58a0ff7249ad960fdbb637299f952bdd43a82f
William Fligor <will@liveify.com>
2014-08-27 19:35:55 -0400

    Added cow

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
-var fortune = require('fortune-teller');
+var fortune = require('fortune-teller'),
+    cowsay = require('cowsay');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
     var fort = fortune.fortune();
+    fort = cowsay.say({
+        'text' : fort,
+        'e': 'oO',
+        'T': 'U'
+    });
     var s = fort.replace(/[^-a-z0-9 ]/ig,'');
     fort = fort.replace(/ /g, "&emsp;");
     fort = fort.replace(/\n/g, "<br />");
